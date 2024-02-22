5825363c5869b59fdb91362be90fc8e8b256659a
codingjamee <codingjamee@gmail.com>
2024-02-23 01:08:26 +0900

    Chore: 프리티어 및 tailwind 세팅
    prettier prettier-plugin-tailwindcss 추가
    tailwind config파일 반응형 미디어쿼리 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,24 +3,27 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/_components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
         'primary-100': '#ADE594',
         'primary-300': '#7BC35B',
         'secondary-100': '#F3D1FF',
         'gray-100': '#EEF2F1',
         'gray-500': '#575C63',
+        'logo-700': '#0E4437',
+        'sub-logo-100': '#8DA69D',
+        'white-100': '#ffffff',
       },
-      fontFamily: {},
+    },
+    screens: {
+      mobile: { max: '767px' },
+      tablet: { min: '768px', max: '1023px' },
+      laptop: { min: '1024px', max: '1279px' },
+      desktop: { min: '1280px' },
     },
   },
   plugins: [],
