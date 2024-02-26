227de5410253ea3a7c44f8a337d7b643566223bc
codingjamee <codingjamee@gmail.com>
2024-02-27 01:44:26 +0900

    Fix: href속성을 주면 Link태그를 사용할 수 있게 설정

--- src/_components/Button.tsx
+++ src/_components/Button.tsx
@@ -1,25 +1,34 @@
+'use client';
+
 import { useButtonProps } from '@/_hooks/useButtonProps';
 import { ButtonPropsType, ButtonVariant } from '@/_types/CommonTypes';
+import Link from 'next/link';
 
 const Button = ({
   href,
   disabled,
   children,
   onClick,
-  as: tagName,
+  // as: tagName,
+  to,
   variant,
   ...props
 }: ButtonPropsType) => {
   const getClassName = (variant: ButtonVariant) => {
     if (variant === 'primary') {
+      if (disabled) {
+        return 'primary-disabled';
+      }
       return 'primary hover:primary-hover';
     } else if (variant === 'outlined') {
+      if (disabled) {
+        return 'outlined-disabled';
+      }
       return 'outlined hover:outlined-hover';
     }
   };
 
   const [buttonProps, { tagName: Component }] = useButtonProps({
-    href,
     disabled,
     onClick,
     ...props,
@@ -27,13 +36,15 @@ const Button = ({
 
   return (
     <>
-      {/* @ts-expect-error Server Component */}
-      <Component
-        className={`button ${getClassName(variant)}`}
-        {...buttonProps}
-        {...props}>
-        {children}
-      </Component>
+      {
+        /* @ts-expect-error Server Component */
+        <Component
+          className={`button ${getClassName(variant)}`}
+          {...buttonProps}
+          {...props}>
+          {href ? <Link href={href}>{children}</Link> : children}
+        </Component>
+      }
     </>
   );
 };
