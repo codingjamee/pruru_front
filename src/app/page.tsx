24facff4717af19e527d9456b1123f15332816c3
codingjamee <codingjamee@gmail.com>
2024-02-27 01:46:28 +0900

    Feat: href속성으로 navigate설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,21 @@ export default function Home() {
             <Image src={logo} alt="logo" />
           </div>
         </div>
-        <div className="flex flex-col justify-center align-middle gap-10">
+        <div className="customflex-column gap-10">
           <h1
-            className={`header big-logo ${montserrat.className} flex justify-center`}>
+            className={`header logo-size desktop:desktop-logo-size tablet:tablet-logo-size ${montserrat.className} flex justify-center`}>
             PRURU
           </h1>
-          <div className="text-center text-sub-logo-100 text-3xl">
+          <div className="text-center text-sub-logo-100 sublogo-size desktop:desktop-sublogo-size tablet:tablet-sublogo-size">
             당신의 냉장고를 신선하게...
           </div>
-          <div className="flex justify-center align-middle gap-14">
-            <Button>로그인</Button>
-            <Button>회원가입</Button>
+          <div className="center-alignment gap-14">
+            <Button variant="primary" href="/welcome/login">
+              로그인
+            </Button>
+            <Button variant="primary" href="/welcome/join">
+              회원가입
+            </Button>
           </div>
         </div>
       </main>
