8132edc5a76b59da6010990035753b30097cb491
codingjamee <codingjamee@gmail.com>
2024-03-03 22:10:52 +0900

    Fix: CarouselProps 이동

--- src/_types/CommonTypes.ts
+++ src/_types/CommonTypes.ts
@@ -1,15 +1,30 @@
 import { ComponentPropsWithoutRef, ReactElement } from 'react';
 
 export type ButtonVariant = 'disabled' | 'primary' | 'outlined' | undefined;
+export type CardVariant = 'primary' | 'outlined' | undefined;
 
 export interface ButtonPropsType extends ComponentPropsWithoutRef<'button'> {
   children?: string | ReactElement | number | string[];
   disabled?: boolean;
   as?: React.ElementType;
+  to?: string;
   href?: string;
   customClassName?: string;
-  onClick: (
+  onClick?: (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => void | undefined | any;
   variant?: ButtonVariant;
 }
+
+export type CarouselProps = {
+  autoPlay?: boolean;
+  stopAutoplayOnHover?: boolean;
+  interval?: number;
+  indicators?: boolean;
+  infiniteLoop?: boolean;
+  height?: number;
+  index?: number;
+  animation?: 'fade' | 'slide';
+  duration?: number;
+  children?: ReactElement | ReactElement[];
+};
