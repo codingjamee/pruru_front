9823adff919674cfb3e548492772e1582cd4a0ae
codingjamee <codingjamee@gmail.com>
2024-03-06 15:18:41 +0900

    Design: carouselItem배치

--- src/_components/CarouselItem.tsx
+++ src/_components/CarouselItem.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+'use client';
+import { ReactElement } from 'react';
 
-function CarouselItem() {
-  return <div>CarouselItem</div>;
-}
+const CarouselItem = ({
+  children,
+}: {
+  children?: ReactElement | ReactElement[];
+}) => {
+  return (
+    <div className="flex max-h-[100%] flex-shrink flex-col">{children}</div>
+  );
+};
 
 export default CarouselItem;
