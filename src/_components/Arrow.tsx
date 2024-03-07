6cbe9e8348483a7b96d8f6c0af887a2332cb7cbb
codingjamee <codingjamee@gmail.com>
2024-03-07 16:59:21 +0900

    Feat: Arrow 컴포넌트 분리

---
+++ src/_components/Arrow.tsx
@@ -0,0 +1,22 @@
+import { Dispatch, SetStateAction } from 'react';
+
+const Arrow = ({
+  direction,
+  executeFn,
+}: {
+  direction: 'left' | 'right';
+  executeFn: () => void;
+}) => {
+  const onClickArrow = () => {
+    executeFn();
+  };
+  return (
+    <div
+      onClick={onClickArrow}
+      className="center-vertical w-[30px] cursor-pointer text-[30px] hover:bg-slate-500">
+      {direction === 'left' ? '<' : '>'}
+    </div>
+  );
+};
+
+export default Arrow;
