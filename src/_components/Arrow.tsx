eb043831b09f07c07790f56df559a9a76b324527
codingjamee <codingjamee@gmail.com>
2024-03-10 00:50:23 +0900

    Fix: hover style 추가, rounded 스타일 설정

--- src/_components/Arrow.tsx
+++ src/_components/Arrow.tsx
@@ -1,19 +1,29 @@
+import { useEffect, useState } from 'react';
+
 const Arrow = ({
   direction,
   executeFn,
   inArrow,
+  hoverStyle,
 }: {
   direction: 'left' | 'right';
   executeFn: () => void;
   inArrow?: boolean;
+  hoverStyle?: string;
 }) => {
   const onClickArrow = () => {
     executeFn();
   };
+  const [hoverColor, setHoverColor] = useState(hoverStyle || undefined);
+
+  useEffect(() => {
+    hoverStyle && setHoverColor(`hover:${hoverStyle}`);
+  }, [hoverStyle]);
+
   return (
     <div
       onClick={onClickArrow}
-      className={`${inArrow ? 'absolute z-50' : ''} ${direction === 'left' ? 'left-0 top-0' : 'right-0 top-0'}  center-vertical flex w-[30px] cursor-pointer text-[30px] hover:bg-slate-500`}>
+      className={`${inArrow ? 'absolute z-50' : ''} ${direction === 'left' ? 'left-0 top-0 rounded-l-lg' : 'right-0 top-0 rounded-r-lg'}  center-vertical flex h-full w-[30px] cursor-pointer text-[30px] ${hoverColor && hoverColor}`}>
       {direction === 'left' ? '<' : '>'}
     </div>
   );
