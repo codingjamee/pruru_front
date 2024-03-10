e1f315082cb916bade722508f1dc12731e750f7d
codingjamee <codingjamee@gmail.com>
2024-03-10 23:03:36 +0900

    Disign: 자주 쓰이는 클래스 따로 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
 import Button from '@/_components/Button';
-import logo from '../../public/pruru_logo.png';
+import logo from '@/_assets/pruru_logo.png';
+
 import Image from 'next/image';
 import { montserrat } from '@/app/layout';
 
 export default function Home() {
   return (
     <>
-      <main className="w-full h-full flex flex-col align-middle justify-center">
+      <main className="full">
         <div className="center-alignment">
-          <div className="logo-img desktop:desktop-logo-img tablet:tablet-logo-img">
+          <div className="logo-img">
             <Image src={logo} alt="logo" />
           </div>
         </div>
-        <div className="customflex-column gap-10">
+        <div className="flex flex-col gap-10">
           <h1
-            className={`header logo-size desktop:desktop-logo-size tablet:tablet-logo-size ${montserrat.className} flex justify-center`}>
+            className={`header logo-size ${montserrat.className} flex justify-center`}>
             PRURU
           </h1>
-          <div className="text-center text-sub-logo-100 sublogo-size desktop:desktop-sublogo-size tablet:tablet-sublogo-size">
+          <div className="text-sub-logo-100 sublogo-size text-center">
             당신의 냉장고를 신선하게...
           </div>
           <div className="center-alignment gap-14">
