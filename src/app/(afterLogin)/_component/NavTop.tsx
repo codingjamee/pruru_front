24c6081fb2c82aa5db4a26462f33c19db8f09b84
codingjamee <codingjamee@gmail.com>
2024-03-10 22:58:35 +0900

    Feat: navTop 설정
    navList를 활용하여 중복되는 로직 단순화
    jsx문법에서 navList와 map을 활용하여 return
    nav.css에 공통되는 class style적용
    hover시에 적용되는 스타일을 설정

--- src/app/(afterLogin)/_component/NavTop.tsx
+++ src/app/(afterLogin)/_component/NavTop.tsx
@@ -1,36 +1,84 @@
 'use client';
 
 import Button from '@/_components/Button';
+import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
-import { useEffect } from 'react';
+
+const navList = [
+  {
+    name: '추가',
+    path: '/add',
+    focusKey: 'add',
+    style: 'link-one',
+    subStyle: 'link-hidden-one',
+    subList: [
+      { name: '식재료', subPath: '/add/food' },
+      { name: '영수증', subPath: '/add/receipt' },
+    ],
+  },
+  {
+    name: '검색',
+    path: '/search',
+    focusKey: 'search',
+    style: 'link-two',
+    subStyle: 'link-hidden-two',
+    subList: [
+      { name: '식재료', subPath: '/search/food' },
+      { name: '영수증', subPath: '/search/receipt' },
+    ],
+  },
+  {
+    name: '식재료',
+    path: '/food',
+    focusKey: 'food',
+    style: 'link-three',
+    subStyle: 'link-hidden-three',
+    subList: [
+      { name: '전체', subPath: '/food?storage=total&sort=category' },
+      { name: '냉장', subPath: '/food?storage=refrigerated&sort=category' },
+      { name: '냉동', subPath: '/food?storage=frozen&sort=category' },
+    ],
+  },
+  {
+    name: '영수증',
+    path: '/receipt',
+    focusKey: 'receipt',
+    subList: [],
+  },
+];
 
 const NavTop = () => {
   const segment = useSelectedLayoutSegment();
+
   console.log(segment);
   const isFocused = (selected: string): string | undefined => {
     if (segment === selected) return 'nav-hover';
     return '';
   };
 
-  useEffect(() => {
-    console.log('컴포넌트 렌더링');
-  }, []);
   return (
-    <ul className="flex h-[72px] items-center justify-between gap-1">
-      <Button
-        href="/add"
-        className={`${isFocused('add')} nav-button hover:nav-hover`}>
-        <li>추가</li>
-      </Button>
-      <Button href="/search" className={`${isFocused('search')} nav-button`}>
-        <li>검색</li>
-      </Button>
-      <Button href="/food" className={`${isFocused('food')} nav-button`}>
-        <li>식재료</li>
-      </Button>
-      <Button href="/receipt" className={`${isFocused('receipt')} nav-button`}>
-        <li>영수증</li>
-      </Button>
+    <ul className="flex h-[52px] items-center justify-between text-size-font-card-title">
+      {navList.map((nav) => (
+        <div key={nav.path} className={`${nav.style} relative`}>
+          <Button
+            href={nav.path}
+            className={`${isFocused(nav.focusKey)}  nav-button hover:nav-hover`}>
+            <li>{nav.name}</li>
+          </Button>
+          <div className={`${nav.subStyle} sub-nav`}>
+            {nav.subList &&
+              nav.subList.length > 0 &&
+              nav.subList.map((sub) => (
+                <Link
+                  key={sub.subPath}
+                  className="link-style"
+                  href={sub.subPath}>
+                  {sub.name}
+                </Link>
+              ))}
+          </div>
+        </div>
+      ))}
     </ul>
   );
 };
