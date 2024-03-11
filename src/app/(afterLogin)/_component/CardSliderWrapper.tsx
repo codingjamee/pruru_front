e9e43e0a94a42d656f15437dad1394a109ac555b
codingjamee <codingjamee@gmail.com>
2024-03-11 15:15:16 +0900

    Fix: card slider 커스텀 class 설정

--- src/app/(afterLogin)/_component/CardSliderWrapper.tsx
+++ src/app/(afterLogin)/_component/CardSliderWrapper.tsx
@@ -2,10 +2,16 @@ import { ReactElement } from 'react';
 
 const CardSliderWrapper = ({
   children,
+  className,
 }: {
   children: ReactElement | ReactElement[];
+  className?: string;
 }) => {
-  return <div className="flex w-full overflow-x-scroll">{children}</div>;
+  return (
+    <div className={`flex w-full overflow-x-scroll ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default CardSliderWrapper;
