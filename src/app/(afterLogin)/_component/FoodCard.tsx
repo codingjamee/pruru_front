95a656a1d39b7f8d034ce24195b6baf4d87f9ed5
codingjamee <codingjamee@gmail.com>
2024-03-11 15:14:32 +0900

    Feat: mobile 버전 FoodCard설정

--- src/app/(afterLogin)/_component/FoodCard.tsx
+++ src/app/(afterLogin)/_component/FoodCard.tsx
@@ -1,5 +1,8 @@
+'use client';
 import CameraSvg from '@/_assets/CameraSvg';
+import { remainedTime } from '@/_utils/remainedTime';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { ReactElement } from 'react';
 
 type foodPropType = {
@@ -8,7 +11,7 @@ type foodPropType = {
   foodTitle?: string;
   purchaseDate?: string;
   foodWeight?: string;
-  expiryDate?: Date;
+  expiryDate?: string;
 };
 
 const FoodCard = ({
@@ -18,32 +21,64 @@ const FoodCard = ({
   children?: ReactElement;
   food: foodPropType;
 }) => {
-  const { imgSrc, foodTitle, purchaseDate, foodWeight } = food;
-  // const nowDate = new Date();
+  const { id, imgSrc, foodTitle, purchaseDate, foodWeight, expiryDate } = food;
+  const remainingDay = expiryDate && remainedTime(expiryDate);
+  const router = useRouter();
+
+  const onClickCard = () => {
+    router.push(`/food/${id}`);
+  };
 
   return (
-    <div className="relative mx-3 flex h-[246px] w-[198px] flex-shrink-0 flex-col rounded-lg border-2 border-solid border-color-default-text shadow-custom">
-      <div className="boder-color-default-text h-[133px] w-full border-b desktop:h-[125px]">
-        <div className="absolute right-0 m-1 flex h-[22px] w-[60px] items-center justify-center rounded-3xl bg-color-secondary-100 text-color-card-text">
-          d-
-        </div>
-        {imgSrc ? (
-          <Image src={imgSrc} alt={imgSrc} />
-        ) : (
-          <div className="h-full w-full p-7">
-            <CameraSvg />
+    <>
+      <div
+        onClick={onClickCard}
+        className="mx-3 flex w-[198px] cursor-pointer flex-col rounded-lg border-2 border-solid border-color-default-text shadow-custom mobile:hidden tablet:h-[246px] desktop:h-[246px]">
+        <div className="boder-color-default-text relative h-full min-w-[194px] tablet:h-[125px] tablet:border-b desktop:h-[133px] desktop:border-b">
+          <div className="absolute right-0 m-1 flex h-[22px] w-[60px] items-center justify-center rounded-3xl bg-color-secondary-100 text-color-card-text">
+            d-{remainingDay}
           </div>
-        )}
+          {imgSrc ? (
+            <Image src={imgSrc} alt={imgSrc} />
+          ) : (
+            <div className="h-full w-full tablet:p-7 desktop:p-7">
+              <CameraSvg />
+            </div>
+          )}
+        </div>
+
+        <div className="h-full w-full flex-col justify-evenly p-[18px] tablet:flex desktop:flex">
+          <div>{foodTitle}</div>
+          <div className="flex w-full justify-between">
+            <div>{purchaseDate}구매</div>
+            <div className="mobile:hidden">{foodWeight}</div>
+          </div>
+        </div>
+        {children && children}
       </div>
-      <div className="flex h-full w-full flex-col justify-evenly p-[18px] ">
-        <div>{foodTitle}</div>
-        <div className="flex w-full justify-between">
-          <div>{purchaseDate}구매</div>
-          <div className="mobile:hidden">{foodWeight}</div>
+      {/* MOBILE */}
+      <div
+        onClick={onClickCard}
+        className="mx-3 flex h-[133px] w-[158px] flex-shrink-0 cursor-pointer flex-col gap-[10px] rounded-lg border-2  border-solid border-color-default-text shadow-custom tablet:hidden desktop:hidden">
+        <div className="relative flex h-full w-full flex-col">
+          <div className="absolute right-0 m-1 flex h-[22px] w-[40px] items-center justify-center rounded-3xl bg-color-secondary-100 text-color-card-text">
+            d-{remainingDay}
+          </div>
+          {imgSrc ? (
+            <Image src={imgSrc} alt={imgSrc} />
+          ) : (
+            <div className="flex h-full w-full items-center justify-center">
+              <div className="w-[40%] pt-[10px]">
+                <CameraSvg />
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="h-full w-full flex-col justify-evenly px-3 pt-2 text-center tablet:hidden desktop:hidden">
+          {foodTitle}
         </div>
       </div>
-      {children && children}
-    </div>
+    </>
   );
 };
 
