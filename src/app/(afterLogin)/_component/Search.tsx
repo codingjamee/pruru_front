c8aa3a078cb95c909d70949ec9d6ecb46f3ff254
codingjamee <codingjamee@gmail.com>
2024-03-12 09:46:15 +0900

    Feat: 검색 UI

---
+++ src/app/(afterLogin)/_component/Search.tsx
@@ -0,0 +1,15 @@
+import SearchGlass from '@/_assets/SearchGlass';
+import Input from '@/_components/Input';
+
+const Search = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <Input placeholder="재료명" />
+      <div className="h-[18px] w-[29px] translate-x-[-30px] translate-y-[-5px] cursor-pointer">
+        <SearchGlass />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
