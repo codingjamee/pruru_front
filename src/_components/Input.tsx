472b09a3425a4b21aa926160ea70c5f67615c3cc
codingjamee <codingjamee@gmail.com>
2024-03-13 17:25:07 +0900

    Feat: variant에 따른 미리 설정된 스타일 추가

--- src/_components/Input.tsx
+++ src/_components/Input.tsx
@@ -1,22 +1,37 @@
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
 type InputProps = ComponentPropsWithRef<'input'>;
+type InputVariantType =
+  | 'primary'
+  | 'outlined'
+  | 'underline'
+  | 'danger'
+  | 'passed';
+interface InputType {
+  type?: string;
+  placeholder?: string;
+  variant: InputVariantType;
+  className?: string;
+}
+type CombinedInputProps = InputProps & InputType;
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  (
-    {
-      type,
-      placeholder,
-      ...props
-    }: {
-      type?: string;
-      placeholder?: string;
-    },
-    ref,
-  ) => {
+const Input = forwardRef<HTMLInputElement, CombinedInputProps>(
+  ({ type, placeholder, variant, className, ...props }: InputType, ref) => {
+    const getClassName = () => {
+      const variantClasses = {
+        primary: 'input-primary',
+        outlined: 'input-outlined',
+        underline: 'input-underline',
+        danger: 'input-danger',
+        passed: 'input-passed',
+      };
+
+      return variant ? variantClasses[variant] : '';
+    };
+
     return (
       <input
-        className="input"
+        className={`input ${variant && getClassName()} ${className}`}
         type={type || 'text'}
         placeholder={placeholder}
         ref={ref}
