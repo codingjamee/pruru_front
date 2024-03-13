3195998e1b87b75486940fe38f9d93ad78d11cb8
codingjamee <codingjamee@gmail.com>
2024-03-14 00:57:32 +0900

    Feat: MSWComponent layout에 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, Montserrat } from 'next/font/google';
 
 import './globals.css';
+import MSWComponent from '@/_components/MSWComponent';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const montserrat = Montserrat({ subsets: ['latin'] });
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MSWComponent />
+        {children}
+      </body>
     </html>
   );
 }
