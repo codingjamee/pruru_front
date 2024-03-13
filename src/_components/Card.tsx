31370d3097e676b061b311109e98942761a656c5
codingjamee <codingjamee@gmail.com>
2024-03-13 17:24:24 +0900

    Fix: card onClick 이벤트 기능 추가

--- src/_components/Card.tsx
+++ src/_components/Card.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { CardVariant } from '@/_types/CommonTypes';
 import { ReactNode } from 'react';
 
@@ -5,18 +6,20 @@ const Card = ({
   children,
   variant,
   className,
+  onClick,
 }: {
   children: ReactNode;
   variant?: CardVariant;
   className?: string;
+  onClick?: () => void;
 }) => {
   const getClassName = (variant: CardVariant) => `card-${variant}`;
   return (
-    <div className="center-alignment">
+    <article className="center-alignment" onClick={onClick}>
       <div className={`card ${getClassName(variant)} ${className} m-1`}>
         {children}
       </div>
-    </div>
+    </article>
   );
 };
 
