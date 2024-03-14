2831cbd05ad864f0b6cae98374d68ccf61a83d53
codingjamee <codingjamee@gmail.com>
2024-03-14 18:00:38 +0900

    Feat: next auth를 위한 auth파일과 middleware설정

---
+++ src/auth.ts
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+} = NextAuth({
+  pages: {
+    signIn: '/welcome/login',
+    newUser: '/welcome/join',
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const authResponse = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/login`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              email: credentials.id,
+              password: credentials.password,
+            }),
+          },
+        );
+        if (!authResponse.ok) {
+          return null;
+        }
+        const user = await authResponse.json();
+        return user;
+      },
+    }),
+  ],
+});
