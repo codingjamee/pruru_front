b7980d1ce57bd039801cbc54946996485d46a89c
codingjamee <codingjamee@gmail.com>
2024-03-16 23:46:37 +0900

    Feat: react-query로 prefetchQuery기능 사용 하여 서버사이드에서 데이터를 프리패칭하고 클라이언트 컴포넌트에 전달

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,11 +2,18 @@ import PlusSvg from '@/_assets/PlusSvg';
 import Button from '@/_components/Button';
 import Carousel from '@/_components/Carousel';
 import CarouselItem from '@/_components/CarouselItem';
-import FoodCard from '../_component/FoodCard';
 import CardSliderWrapper from '../_component/CardSliderWrapper';
 import Link from 'next/link';
-import SmallFoodCard from '../_component/SmallFoodCard';
 import React from 'react';
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query';
+import ExpireyFood from '../_component/ExpireyFood';
+import { foodsByPurchase, getFoodsByExpiry } from '@/_utils/getQuery';
+import RecentlyFood from '../_component/RecentlyFood';
+
 export const foodCardDummyArr = [
   {
     id: 1,
@@ -49,65 +56,64 @@ export const foodCardDummyArr = [
     expiryDate: '2024-03-30T15:00:00Z',
   },
 ];
-const page = () => {
+
+const page = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['foods', 'expiryDate'],
+    queryFn: getFoodsByExpiry,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ['foods', 'purchaseDate'],
+    queryFn: foodsByPurchase,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <>
-      <Carousel
-        indicators={false}
-        customClass="my-[63px] h-[168px] tablet:h-[225px] desktop:h-[275px]"
-        arrowHoverStyle="bg-color-primary-m">
-        <CarouselItem>
-          <div className="m-[18px] flex h-full flex-col gap-4 tablet:m-[30px] desktop:m-[40px]">
-            <div className="text-size-font-card-title">식재료 추가하기</div>
-            <Button
-              href="/add/food"
-              className="flex h-[50%] w-full flex-col items-center justify-center rounded-lg hover:bg-color-primary-m">
-              <PlusSvg className="flex h-[44px] w-[44px] items-center justify-center" />
-            </Button>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div className="m-[18px] flex h-full flex-col gap-4 tablet:m-[30px] desktop:m-[40px]">
-            <div className="text-size-font-card-title">영수증 추가하기</div>
-            <Button
-              href="/add/receipt"
-              className="flex h-[50%] w-full flex-col items-center justify-center rounded-lg hover:bg-color-primary-m">
-              <PlusSvg className="h-[44px] w-[44px]" />
-            </Button>
-          </div>
-        </CarouselItem>
-      </Carousel>
+      <HydrationBoundary state={dehydratedState}>
+        <Carousel
+          indicators={false}
+          customClass="my-[63px] h-[168px] tablet:h-[225px] desktop:h-[275px]"
+          arrowHoverStyle="bg-color-primary-m">
+          <CarouselItem>
+            <div className="m-[18px] flex h-full flex-col gap-4 tablet:m-[30px] desktop:m-[40px]">
+              <div className="text-size-font-card-title">식재료 추가하기</div>
+              <Button
+                href="/add/food"
+                className="flex h-[50%] w-full flex-col items-center justify-center rounded-lg hover:bg-color-primary-m">
+                <PlusSvg className="flex h-[44px] w-[44px] items-center justify-center" />
+              </Button>
+            </div>
+          </CarouselItem>
+          <CarouselItem>
+            <div className="m-[18px] flex h-full flex-col gap-4 tablet:m-[30px] desktop:m-[40px]">
+              <div className="text-size-font-card-title">영수증 추가하기</div>
+              <Button
+                href="/add/receipt"
+                className="flex h-[50%] w-full flex-col items-center justify-center rounded-lg hover:bg-color-primary-m">
+                <PlusSvg className="h-[44px] w-[44px]" />
+              </Button>
+            </div>
+          </CarouselItem>
+        </Carousel>
 
-      <div className="my-10 flex justify-between text-size-font-card-title">
-        <div>유통기한 임박</div>
-        <Link href="/food?storage=total&sort=expiryDate">더보기</Link>
-      </div>
-      <CardSliderWrapper>
-        {foodCardDummyArr.map((food) => (
-          <div key={food.id}>
-            <FoodCard className="mobile:hidden" food={food} />
-            <SmallFoodCard
-              className="tablet:hidden desktop:hidden"
-              food={food}
-            />
-          </div>
-        ))}
-      </CardSliderWrapper>
-      <div className="my-10 flex justify-between text-size-font-card-title">
-        <div>최근 산 재료</div>
-        <Link href="/food?storage=total&sort=purchaseDate">더보기</Link>
-      </div>
-      <CardSliderWrapper>
-        {foodCardDummyArr.map((food) => (
-          <React.Fragment key={food.id}>
-            <FoodCard className="mobile:hidden" food={food} />
-            <SmallFoodCard
-              className="tablet:hidden desktop:hidden"
-              food={food}
-            />
-          </React.Fragment>
-        ))}
-      </CardSliderWrapper>
+        <div className="my-10 flex justify-between text-size-font-card-title">
+          <div>유통기한 임박</div>
+          <Link href="/food?storage=total&sort=expiryDate">더보기</Link>
+        </div>
+        <CardSliderWrapper>
+          <ExpireyFood />
+        </CardSliderWrapper>
+        <div className="my-10 flex justify-between text-size-font-card-title">
+          <div>최근 산 재료</div>
+          <Link href="/food?storage=total&sort=purchaseDate">더보기</Link>
+        </div>
+        <CardSliderWrapper>
+          <RecentlyFood />
+        </CardSliderWrapper>
+      </HydrationBoundary>
     </>
   );
 };
