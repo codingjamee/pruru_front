2ec1fd441fc3109d92d599b07504f0074fb13fb7
codingjamee <codingjamee@gmail.com>
2024-03-18 12:55:36 +0900

    Fix: pathname검사 및 describe 수정

--- cypress/e2e/loginTest.cy.ts
+++ cypress/e2e/loginTest.cy.ts
@@ -1,15 +1,18 @@
-describe('template spec', () => {
-  before(() => {
+describe('welcome page', () => {
+  beforeEach(() => {
     cy.log(`Visiting http://localhost:3000`);
     cy.visit('http://localhost:3000');
+    cy.clearCookies();
   });
 
   it('visit to login page if click login button', () => {
     cy.contains('button', '로그인').click();
+    cy.location('pathname').should('eq', '/welcome/login');
     cy.contains('h1', '로그인');
   });
   it('visit to login page if click login button', () => {
     cy.contains('button', '회원가입').click();
+    cy.location('pathname').should('eq', '/welcome/join');
     cy.contains('h1', '회원가입');
   });
 });
