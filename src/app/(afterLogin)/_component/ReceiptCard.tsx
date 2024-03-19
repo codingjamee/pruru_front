0e31cc71eb8706b8d30090139f8ccb3c85509509
codingjamee <codingjamee@gmail.com>
2024-03-19 14:05:41 +0900

    Feat: query로 요청 받아온 결과 card로 렌더링

--- src/app/(afterLogin)/_component/ReceiptCard.tsx
+++ src/app/(afterLogin)/_component/ReceiptCard.tsx
@@ -2,27 +2,39 @@
 import Card from '@/_components/Card';
 import { ReceiptArrType } from '@/_types/ReceiptTypes';
 import { useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+import { getReceiptsByMonth } from '@/_utils/getQuery';
 
-const ReceiptCard = ({ receipt }: { receipt: ReceiptArrType }) => {
+const ReceiptCard = ({ YM }: { YM?: string }) => {
   const router = useRouter();
+  const { data: receipts } = useQuery({
+    queryKey: ['receipt', 'monthly', YM],
+    queryFn: () => getReceiptsByMonth(YM),
+    staleTime: 60 * 1000,
+  });
   const onClickCard = () => {
-    router.push(`/receipt/${receipt.receipt_id.toString()}`);
+    router.push(`/receipt/${receipts.receipt_id.toString()}`);
   };
   return (
-    <Card
-      onClick={onClickCard}
-      key={receipt.receipt_id}
-      variant="primary"
-      className="flex w-full flex-shrink cursor-pointer justify-between gap-[20px] px-[40px]">
-      <div className="flex w-full justify-between">
-        <div>{receipt.purchase_date} 구매 영수증</div>
-        <div>{receipt.purchase_location}</div>
-      </div>
-      <div className="flex w-full justify-between">
-        <div>품목 {receipt.quantity}개</div>
-        <div>총 {receipt.total_price.toLocaleString()}원</div>
-      </div>
-    </Card>
+    <>
+      {receipts &&
+        receipts.map((receipt: ReceiptArrType) => (
+          <Card
+            onClick={onClickCard}
+            key={receipt.receipt_id}
+            variant="outlined"
+            className="flex w-full flex-shrink cursor-pointer justify-between gap-[20px] px-[40px]">
+            <div className="flex w-full justify-between">
+              <div>{receipt.purchase_date} 구매 영수증</div>
+              <div>{receipt.purchase_location}</div>
+            </div>
+            <div className="flex w-full justify-between">
+              <div>품목 {receipt.quantity}개</div>
+              <div>총 {receipt.total_price.toLocaleString()}원</div>
+            </div>
+          </Card>
+        ))}
+    </>
   );
 };
 
