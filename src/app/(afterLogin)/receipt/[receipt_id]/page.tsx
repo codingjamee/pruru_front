530d9163bda1c09d36fe5e914fe0a8e78bac8780
codingjamee <codingjamee@gmail.com>
2024-03-19 17:26:10 +0900

    Fix: 폴더명 변경 receiptId => receipt_id

--- src/app/(afterLogin)/receipt/[receipt_id]/page.tsx
+++ src/app/(afterLogin)/receipt/[receipt_id]/page.tsx
@@ -1,6 +1,7 @@
 import ReceiptDetailCard from '../../_component/ReceiptDetailCard';
 
 const page = ({ params }: { params: { receipt_id: string } }) => {
+  console.log(params);
   return <ReceiptDetailCard receipt_id={params.receipt_id} />;
 };
 
