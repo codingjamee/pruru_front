3d3bf261bc64e5e26956daec3b8808e67c6c0453
codingjamee <codingjamee@gmail.com>
2024-03-19 15:32:47 +0900

    Fix: dummy receipt array 삭제 mock server 로직 구현으로 인하여

--- src/app/(afterLogin)/receipt/page.tsx
+++ src/app/(afterLogin)/receipt/page.tsx
@@ -4,38 +4,7 @@ import Card from '@/_components/Card';
 import React from 'react';
 import ReceiptArrowFn from '../_component/ReceiptArrowFn';
 import ReceiptCard from '../_component/ReceiptCard';
-import { ReceiptArrType } from '@/_types/ReceiptTypes';
 
-export const receiptDummyArr: ReceiptArrType[] = [
-  {
-    receipt_id: 1,
-    quantity: 10,
-    purchase_location: '이마트',
-    purchase_date: '24.2.17',
-    total_price: 20950,
-  },
-  {
-    receipt_id: 2,
-    quantity: 17,
-    purchase_location: '시장마트',
-    purchase_date: '24.2.27',
-    total_price: 52182,
-  },
-  {
-    receipt_id: 3,
-    quantity: 1,
-    purchase_location: '편의점',
-    purchase_date: '24.2.9',
-    total_price: 5931,
-  },
-  {
-    receipt_id: 4,
-    quantity: 1,
-    purchase_location: '홈플러스',
-    purchase_date: '24.2.29',
-    total_price: 5931,
-  },
-];
 const page = () => {
   return (
     <div className="flex w-full flex-shrink flex-col gap-[20px] pt-[20px]">
@@ -53,9 +22,7 @@ const page = () => {
           <PlusSvg className="flex h-[44px] w-[44px] items-center justify-center" />
         </Button>
       </Card>
-      {receiptDummyArr.map((receipt) => (
-        <ReceiptCard key={receipt.receipt_id} receipt={receipt} />
-      ))}
+      <ReceiptCard />
     </div>
   );
 };
