ae583ed6186a8dd348ecfcfcc84975ec6431e263
codingjamee <codingjamee@gmail.com>
2024-03-19 14:04:10 +0900

    Feat: /receipt으로 요청기능 및 요청  보낼때 year과 month없으면 query로 현재로 기본설정

--- src/_utils/getQuery.ts
+++ src/_utils/getQuery.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { api } from './createCustomFetch';
 
 export const getFoodsByExpiry = async () => {
@@ -16,6 +17,21 @@ export const foodsByPurchase = async () => {
   const res = await api(`/food?storage=total&sort=purchase_date`, {
     next: {
       tags: ['foods', 'purchase_date'],
+    },
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+};
+
+export const getReceiptsByMonth = async (YM?: string) => {
+  if (!YM) {
+    YM = dayjs().format('YY.MM');
+  }
+  const res = await api(`/receipt?month=${YM}`, {
+    next: {
+      tags: ['receipt', 'monthly'],
     },
   });
   if (!res.ok) {
