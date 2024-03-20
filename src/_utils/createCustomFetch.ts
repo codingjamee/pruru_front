03d2bdbb5aa6ecfb24d7e17776d4023ccace8a02
codingjamee <codingjamee@gmail.com>
2024-03-20 14:21:04 +0900

    Feat: CLOVA 요청을 위해 customFetch 만들기
    CLOVA로 영수증 분석 요청시
    body포맷이 정해져 있으므로 baseBody가 있어야 함
    기존의 createCustomFetch함수로는 커스텀 객체를 만드는데 한계가 있다고 판단하여
    createCustomReceiptFetch 함수를 만들어 receiptApi요청 객체를 생성하도록 만듦
    dynamicValues로 요청 객체의 format, url, imestamp, data를 커스텀하게 줄 수있게
    uuid등 필요 필드값 추가

--- src/_utils/createCustomFetch.ts
+++ src/_utils/createCustomFetch.ts
@@ -1,20 +1,45 @@
+import uuid4 from 'uuid4';
+
 interface HeadersObjType {
-  [key: string]: string;
+  [key: string]: string | undefined;
 }
 
-interface CustomFetchTypes {
-  baseURL: string;
+interface ImageType {
+  format: string;
+  name: string;
+  url?: string;
+  data?: string | ArrayBuffer | null | undefined;
+}
+
+interface CustomFetchType {
+  baseURL?: string;
   headers: HeadersObjType;
+  'X-OCR-SECRET'?: string;
 }
 
-const createCustomFetch = ({ baseURL, headers }: CustomFetchTypes) => {
+interface DynamicValuesType {
+  format?: string;
+  imageUrl?: string;
+  timestamp?: number;
+  data?: string | ArrayBuffer | null | undefined;
+}
+
+const createCustomFetch = ({ baseURL, headers }: CustomFetchType) => {
   return async (url = '', options?: RequestInit): Promise<Response> => {
     const baseFullUrl = `${baseURL}${url}`;
-    const customHeaders = { ...headers, ...(options?.headers || {}) };
-
+    const customHeaders = Object.entries(headers).reduce(
+      (acc, [key, value]) => {
+        if (value !== undefined) {
+          acc[key] = value;
+        }
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
     let response;
     try {
       response = await fetch(baseFullUrl, {
+        ...options,
         headers: customHeaders,
       });
     } catch (err) {
@@ -30,5 +55,74 @@ export const api = createCustomFetch({
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${process.env.API_TOKEN as string}`,
+  },
+});
+
+const createCustomReceiptFetch = ({ baseURL, headers }: CustomFetchType) => {
+  return async (
+    url = '',
+    options?: RequestInit,
+    dynamicValues?: DynamicValuesType,
+  ): Promise<Response> => {
+    const baseFullUrl = `${baseURL}${url}`;
+    const customHeaders = Object.entries(headers).reduce(
+      (acc, [key, value]) => {
+        if (value !== undefined) {
+          acc[key] = value;
+        }
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+    const baseBody = {
+      images: [
+        {
+          format: 'png',
+          name: 'medium',
+          data: undefined,
+          url: undefined,
+        },
+      ] as ImageType[],
+      lang: 'ko',
+      requestId: uuid4(),
+      resultType: 'string',
+      timestamp: 0,
+      version: 'V2',
+    };
+
+    if (dynamicValues) {
+      if (dynamicValues.format) {
+        baseBody.images[0].format = dynamicValues.format;
+      }
+      if (dynamicValues.imageUrl) {
+        baseBody.images[0].url = dynamicValues.imageUrl;
+      }
+      if (dynamicValues.timestamp) {
+        baseBody.timestamp = dynamicValues.timestamp;
+      }
+      if (dynamicValues.data) {
+        baseBody.images[0].data = dynamicValues.data;
+      }
+    }
+    let response;
+    try {
+      response = await fetch(baseFullUrl, {
+        ...options,
+        headers: customHeaders,
+        body: JSON.stringify(baseBody),
+      });
+    } catch (err) {
+      console.error(err);
+      throw new Error('error occured!');
+    }
+    return response;
+  };
+};
+
+export const receiptApi = createCustomReceiptFetch({
+  baseURL: process.env.CLOVA_REQUEST_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-OCR-SECRET': process.env.CLOVA_SECRET_KEY,
   },
 });
