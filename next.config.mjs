c3bb8b467aca55576e168cbb06f07b067eb53d29
codingjamee <codingjamee@gmail.com>
2024-03-21 10:14:31 +0900

    Feat:proxy설정 완료 next auth는 되지 않는 상태 next auth도 proxy설정 따로 필요

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: false,
   images: {
     domains: ['loremflickr.com', 'localhost'],
   },
+  rewrites: async () => ({
+    beforeFiles: [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
+      },
+      {
+        source: '/custom/:path*',
+        destination: `${process.env.NEXT_PUBLIC_CLOVA_REQUEST_URL}/custom/:path*`,
+      },
+    ],
+  }),
 };
 
 export default nextConfig;
