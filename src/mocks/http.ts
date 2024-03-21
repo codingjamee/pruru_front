9c39a2c3fd481c63bbbf6cf218f32d31f26b364e
codingjamee <codingjamee@gmail.com>
2024-03-21 10:15:12 +0900

    Fix: proxy설정으로 mockserver cors설정 해제

--- src/mocks/http.ts
+++ src/mocks/http.ts
@@ -1,18 +1,9 @@
 import { createMiddleware } from '@mswjs/http-middleware';
 import express from 'express';
-import cors from 'cors';
 import { handlers } from './handlers';
 
 const app = express();
 const port = 9090;
-
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200,
-    credentials: true,
-  }),
-);
 
 app.use(express.json());
 app.use(createMiddleware(...handlers));
