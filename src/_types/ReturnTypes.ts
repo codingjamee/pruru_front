b5902e6c4ec08d7eb2d0fe3b89fa4dca202f7768
codingjamee <codingjamee@gmail.com>
2024-03-22 17:19:28 +0900

    Feat: analyzedReceiptType추가

--- src/_types/ReturnTypes.ts
+++ src/_types/ReturnTypes.ts
@@ -1,9 +1,75 @@
 export interface AnaylzedNameTypes {
-  name: {
-    formatted: { value: string };
-  };
+  formatted: { value: string };
 }
 
 export type ReturnItemTypes = {
-  items: AnaylzedNameTypes;
+  name: AnaylzedNameTypes;
 };
+
+export interface ModifiedAnalyzeReceiptType {
+  food_name: string;
+  purchase_price: string;
+  quantity: string;
+}
+
+export interface AnalyzedReceiptAllType {
+  name: {
+    text: string;
+    formatted: {
+      value: string;
+    };
+    keyText: string;
+    confidenceScore: number;
+    boundingPolys: Array<{
+      vertices: Array<{
+        x: number;
+        y: number;
+      }>;
+    }>;
+    maskingPolys: Array<unknown>;
+  };
+  count: {
+    text: string;
+    formatted: {
+      value: string;
+    };
+    keyText: string;
+    confidenceScore: number;
+    boundingPolys: Array<{
+      vertices: Array<{
+        x: number;
+        y: number;
+      }>;
+    }>;
+  };
+  price: {
+    price: {
+      text: string;
+      formatted: {
+        value: string;
+      };
+      keyText: string;
+      confidenceScore: number;
+      boundingPolys: Array<{
+        vertices: Array<{
+          x: number;
+          y: number;
+        }>;
+      }>;
+    };
+    unitPrice?: {
+      text: string;
+      formatted: {
+        value: string;
+      };
+      keyText: string;
+      confidenceScore: number;
+      boundingPolys: Array<{
+        vertices: Array<{
+          x: number;
+          y: number;
+        }>;
+      }>;
+    };
+  };
+}
