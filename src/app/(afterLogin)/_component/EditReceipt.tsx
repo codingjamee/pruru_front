80c9393a7132b896419e6720da890189e6cef65d
Jamee <codingjamee@gmail.com>
2024-03-24 02:01:34 +0900

    Fix: 영수증 데이터 기본값으로 가져오는 로직
    allSearchResults로 캐싱되어있던 영수증 인식 데이터
    form의 defaultValues로 가져오기
    purchase_price를 변경하면 watch하고 있던 값들이 계산되어
    setTotalPrice하기

--- src/app/(afterLogin)/_component/EditReceipt.tsx
+++ src/app/(afterLogin)/_component/EditReceipt.tsx
@@ -2,35 +2,78 @@
 import Button from '@/_components/Button';
 import Card from '@/_components/Card';
 import PlusSvg from '@/_assets/PlusSvg';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import MinusSvg from '@/_assets/MinusSvg';
 import Input from '@/_components/Input';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { purchaseReceiptInfo } from '@/mocks/data';
 import { useQueryClient } from '@tanstack/react-query';
+import {
+  PurchaseReceiptInfoType,
+  ReceiptDetailType,
+} from '@/_types/ReceiptTypes';
 
 const EditReceipt = () => {
   //receipt정보가 있다면 가져와서 데이터 input의 value로 설정해주기
   const queryClient = useQueryClient();
-  const foundReceiptData = queryClient.getQueryData(['allSearchResults']);
+  const foundReceiptData: PurchaseReceiptInfoType | undefined =
+    queryClient.getQueryData(['allSearchResults']);
+  const [prev, setPrev] = useState<number[] | []>([]);
+  const [totalPrice, setTotalPrice] = useState(
+    (foundReceiptData &&
+      foundReceiptData.receiptItems.reduce(
+        (acc: number, cur: ReceiptDetailType) => {
+          return ((cur.purchase_price && cur?.purchase_price) || 0) + acc;
+        },
+        0,
+      )) ||
+      0,
+  );
 
   useEffect(() => {
     if (foundReceiptData) console.log(foundReceiptData);
   }, [foundReceiptData]);
 
-  const total_price = useMemo(
-    () =>
-      purchaseReceiptInfo[0].receiptItems.reduce((acc, cur) => {
-        return cur.purchase_price * cur.quantity + acc;
-      }, 0),
-    [purchaseReceiptInfo],
-  );
-
-  const { control, handleSubmit, register } = useForm({
+  const { control, handleSubmit, register, watch } = useForm({
     defaultValues: {
-      receiptArr: [...purchaseReceiptInfo[0].receiptItems],
+      receiptArr:
+        foundReceiptData &&
+        foundReceiptData.receiptItems.map((data) => {
+          return {
+            food_category: data?.food_category || '',
+            food_name: data?.food_name || '',
+            purchase_price: data?.purchase_price || 0,
+            food_weight: '',
+            food_id: Math.random() * 4,
+            quantity: data?.quantity,
+            registered: false,
+          };
+        }),
     },
   });
+
+  const watchArr = watch('receiptArr');
+
+  useEffect(() => {
+    const currentPrices =
+      watchArr && watchArr.map((field) => field.purchase_price);
+
+    // 이전 value 값들과 현재 value 값들을 비교하여 변경점 확인
+    const isValueChanged =
+      currentPrices &&
+      currentPrices.some((value, index) => value !== prev[index]);
+
+    if (isValueChanged) {
+      // value 값에 변경이 있을 때만 합계 계산
+      const totalSum = currentPrices.reduce(
+        (acc, value) => acc + (Number(value) || 0),
+        0,
+      );
+      setTotalPrice(totalSum);
+      // 현재 value 값들을 이전 값으로 설정
+      setPrev(currentPrices);
+    }
+  }, [watchArr]);
 
   const { fields, append, remove } = useFieldArray({
     control,
@@ -50,10 +93,10 @@ const EditReceipt = () => {
         className="flex min-h-[695px] w-[635px] flex-col gap-[30px] px-[55px] mobile:min-h-[450px] mobile:border-0 mobile:px-4">
         <div className="flex w-full justify-between gap-[10px] mobile:flex-col">
           <div className="w-[213px] rounded-lg border border-solid border-color-default-text px-[30px] py-[7px] text-center mobile:w-full">
-            {purchaseReceiptInfo[0].purchase_date} 구매
+            {(foundReceiptData && foundReceiptData.purchase_date) || ''} 구매
           </div>
           <Button variant="primary" className="rounded-lg mobile:w-full">
-            {purchaseReceiptInfo[0].purchase_location}
+            {(foundReceiptData && foundReceiptData.purchase_location) || ''}
           </Button>
         </div>
         <div className="flex flex-1 flex-col gap-[10px] mobile:gap-[15px]">
@@ -65,7 +108,7 @@ const EditReceipt = () => {
             <div className="basis-1/12">등록</div>
           </div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            {fields.map((receipt, index) => (
+            {fields.map((_, index) => (
               <div
                 className="flex w-full items-center justify-between gap-[5px] truncate"
                 key={`receipt-${index}`}>
@@ -79,7 +122,6 @@ const EditReceipt = () => {
                   {...register(`receiptArr.${index}.food_name`)}
                   className="max-w-[127px] basis-5/12 truncate"
                 />
-
                 <Input
                   variant="underline"
                   {...register(`receiptArr.${index}.food_weight`)}
@@ -87,19 +129,15 @@ const EditReceipt = () => {
                 />
                 <Input
                   variant="underline"
-                  defaultValue={(
-                    receipt.price_per_amount * receipt.quantity
-                  ).toLocaleString()}
+                  {...register(`receiptArr.${index}.purchase_price`)}
                   className="basis-2/12"
                 />
-
                 <div className="basis-1/12 cursor-pointer">
                   <MinusSvg onClick={() => remove(index)} />
                 </div>
               </div>
             ))}
           </form>
-
           <div className="flex gap-[5px]">
             <div className="basis-11/12"></div>
             <PlusSvg
@@ -111,7 +149,6 @@ const EditReceipt = () => {
                   food_name: '',
                   food_weight: '',
                   purchase_price: 0,
-                  price_per_amount: 0,
                   quantity: 0,
                   registered: false,
                 })
@@ -119,12 +156,10 @@ const EditReceipt = () => {
             />
           </div>
         </div>
-
         <div className="flex justify-end gap-[30px]">
           <div>품목 {purchaseReceiptInfo[0].receiptItems.length}개</div>
-          <div>총{total_price.toLocaleString()}원</div>
+          <div>총{totalPrice.toLocaleString()}원</div>
         </div>
-
         <Button
           onClick={onSubmit}
           variant="outlined"
