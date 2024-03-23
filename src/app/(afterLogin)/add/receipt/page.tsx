6b2161f8fa808def585fea4f77d3b1dddc9dfe3a
Jamee <codingjamee@gmail.com>
2024-03-24 01:35:12 +0900

    Rename: UploadReceipt => AnalyzeReceipt 파일명 변경

--- src/app/(afterLogin)/add/receipt/page.tsx
+++ src/app/(afterLogin)/add/receipt/page.tsx
@@ -1,7 +1,7 @@
 import PlusSvg from '@/_assets/PlusSvg';
 import Button from '@/_components/Button';
 import Card from '@/_components/Card';
-import UploadReceipt from '../../_component/UploadReceipt';
+import AnalyzeReceipt from '../../_component/AnalyzeReceipt';
 
 const page = () => {
   return (
@@ -12,7 +12,7 @@ const page = () => {
       <Card
         variant="outlined"
         className="flex min-h-[695px] w-[635px] flex-col gap-[30px] px-[55px] mobile:min-h-[450px] mobile:border-0 mobile:px-4">
-        <UploadReceipt />
+        <AnalyzeReceipt />
         <div className="flex h-full flex-grow flex-col gap-4 rounded-lg border p-[12px]">
           <div className="p-[8px] text-size-font-card-title">직접 입력하기</div>
           <Button
