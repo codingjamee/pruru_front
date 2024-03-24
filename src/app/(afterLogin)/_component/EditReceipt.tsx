d65ce6ca7238eb3a52bc70cd932a51a63236063e
Jamee <codingjamee@gmail.com>
2024-03-24 23:57:51 +0900

    Feat: 영수증 수정 기능 구현완료 post직전까지 1. defaultValues의 field 맞추기 구매처와 구매일자 . 구매일자의 기본값 설정 분석된 영수증 데이터는 receiptItems에 2. 만일 분석된 영수증 데이터가 없다면 기본 form object한개 설정 3. useFieldArray로 객체 내 중첩된 배열 관리 4. watch기능으로 form 동적 추가시 length 렌더링 5. 사용자가 금액 입력시 총 금액 수정 렌더링 6. 클린업에서 unsubscribe를 호출하여 메모리 누수 방지 7. form 구조 변경으로 인한 form태그 위치 변경 8. Input 컴포넌트 validation 추가     에러가 있다면 variant danger로 하여 유저에게 빈값 알림 설정

--- src/app/(afterLogin)/_component/EditReceipt.tsx
+++ src/app/(afterLogin)/_component/EditReceipt.tsx
@@ -6,81 +6,80 @@ import { useEffect, useState } from 'react';
 import MinusSvg from '@/_assets/MinusSvg';
 import Input from '@/_components/Input';
 import { useFieldArray, useForm } from 'react-hook-form';
-import { purchaseReceiptInfo } from '@/mocks/data';
 import { useQueryClient } from '@tanstack/react-query';
-import {
-  PurchaseReceiptInfoType,
-  ReceiptDetailType,
-} from '@/_types/ReceiptTypes';
+import { PurchaseReceiptInfoType } from '@/_types/ReceiptTypes';
+import dayjs from 'dayjs';
 
 const EditReceipt = () => {
-  //receipt정보가 있다면 가져와서 데이터 input의 value로 설정해주기
   const queryClient = useQueryClient();
   const foundReceiptData: PurchaseReceiptInfoType | undefined =
     queryClient.getQueryData(['allSearchResults']);
-  const [prev, setPrev] = useState<number[] | []>([]);
-  const [totalPrice, setTotalPrice] = useState(
-    (foundReceiptData &&
-      foundReceiptData.receiptItems.reduce(
-        (acc: number, cur: ReceiptDetailType) => {
-          return ((cur.purchase_price && cur?.purchase_price) || 0) + acc;
-        },
-        0,
-      )) ||
-      0,
-  );
+  const [length, setLength] = useState<number | undefined>(0);
+  const [totalPrice, setTotalPrice] = useState<number | undefined>(0);
 
-  useEffect(() => {
-    if (foundReceiptData) console.log(foundReceiptData);
-  }, [foundReceiptData]);
-
-  const { control, handleSubmit, register, watch } = useForm({
+  const {
+    control,
+    handleSubmit,
+    register,
+    watch,
+    formState: { errors },
+  } = useForm<PurchaseReceiptInfoType>({
     defaultValues: {
-      receiptArr:
-        foundReceiptData &&
-        foundReceiptData.receiptItems.map((data) => {
-          return {
-            food_category: data?.food_category || '',
-            food_name: data?.food_name || '',
-            purchase_price: data?.purchase_price || 0,
-            food_weight: '',
-            food_id: Math.random() * 4,
-            quantity: data?.quantity,
-            registered: false,
-          };
-        }),
+      purchase_location:
+        (foundReceiptData && foundReceiptData.purchase_location) ||
+        '구매처 입력란',
+      purchase_date:
+        dayjs(foundReceiptData && foundReceiptData.purchase_date).format(
+          'YY.MM.DD',
+        ) + ' 구매',
+      receiptItems: foundReceiptData
+        ? foundReceiptData.receiptItems.map((data) => {
+            return {
+              food_category: data?.food_category || '',
+              food_name: data?.food_name || '',
+              purchase_price: data?.purchase_price || 0,
+              food_weight: '',
+              food_id: Math.random() * 4,
+              quantity: data?.quantity,
+              registered: false,
+            };
+          })
+        : [
+            {
+              food_id: Math.random() * 4,
+              food_category: undefined,
+              food_name: undefined,
+              food_weight: undefined,
+              purchase_price: undefined,
+              quantity: undefined,
+              registered: false,
+            },
+          ],
     },
   });
 
-  const watchArr = watch('receiptArr');
-
-  useEffect(() => {
-    const currentPrices =
-      watchArr && watchArr.map((field) => field.purchase_price);
-
-    // 이전 value 값들과 현재 value 값들을 비교하여 변경점 확인
-    const isValueChanged =
-      currentPrices &&
-      currentPrices.some((value, index) => value !== prev[index]);
-
-    if (isValueChanged) {
-      // value 값에 변경이 있을 때만 합계 계산
-      const totalSum = currentPrices.reduce(
-        (acc, value) => acc + (Number(value) || 0),
-        0,
-      );
-      setTotalPrice(totalSum);
-      // 현재 value 값들을 이전 값으로 설정
-      setPrev(currentPrices);
-    }
-  }, [watchArr]);
-
   const { fields, append, remove } = useFieldArray({
     control,
-    name: 'receiptArr',
+    name: 'receiptItems',
   });
 
-  const onSubmit = () => {};
+  useEffect(() => {
+    const { unsubscribe } = watch((value) => {
+      setLength(value.receiptItems && value.receiptItems.length);
+      setTotalPrice(
+        value.receiptItems &&
+          value.receiptItems.reduce(
+            (acc: number, cur) => acc + (Number(cur?.purchase_price) || 0),
+            0,
+          ),
+      );
+    });
+    return () => unsubscribe();
+  }, [watch]);
+
+  const onSubmit = (data: PurchaseReceiptInfoType) => {
+    console.log(data);
+  };
 
   return (
     <div className="py-[20px] mobile:py-[10px]">
@@ -91,81 +90,134 @@ const EditReceipt = () => {
       <Card
         variant="outlined"
         className="flex min-h-[695px] w-[635px] flex-col gap-[30px] px-[55px] mobile:min-h-[450px] mobile:border-0 mobile:px-4">
-        <div className="flex w-full justify-between gap-[10px] mobile:flex-col">
-          <div className="w-[213px] rounded-lg border border-solid border-color-default-text px-[30px] py-[7px] text-center mobile:w-full">
-            {(foundReceiptData && foundReceiptData.purchase_date) || ''} 구매
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-[30px]">
+          <div className="flex w-full justify-between gap-[10px] mobile:flex-col">
+            <Input
+              variant={errors.purchase_date ? 'danger' : 'outlined'}
+              className="w-[213px] rounded-lg border border-solid border-color-default-text px-[30px] py-[7px] text-center mobile:w-full"
+              {...register('purchase_date', {
+                required: '빈 칸이 없게 작성해주세요',
+                minLength: 1,
+              })}
+            />
+            <Input
+              variant="primary"
+              className="w-[213px] rounded-lg text-center mobile:w-full"
+              {...register('purchase_location', {
+                required: '빈 칸이 없게 작성해주세요',
+                minLength: 1,
+              })}
+            />
           </div>
-          <Button variant="primary" className="rounded-lg mobile:w-full">
-            {(foundReceiptData && foundReceiptData.purchase_location) || ''}
-          </Button>
-        </div>
-        <div className="flex flex-1 flex-col gap-[10px] mobile:gap-[15px]">
-          <div className="flex w-full justify-between text-[14px]">
-            <div className="basis-2/12">카테고리</div>
-            <div className="max-w-[127px] basis-5/12">품명</div>
-            <div className="basis-2/12">중량</div>
-            <div className="basis-2/12">금액</div>
-            <div className="basis-1/12">등록</div>
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-1 flex-col gap-[10px] mobile:gap-[15px]">
+            <div className="flex w-full justify-between text-[14px]">
+              <div className="basis-2/12">카테고리</div>
+              <div className="max-w-[127px] basis-5/12">품명</div>
+              <div className="basis-2/12">중량</div>
+              <div className="basis-2/12">금액</div>
+              <div className="basis-1/12">등록</div>
+            </div>
             {fields.map((_, index) => (
               <div
                 className="flex w-full items-center justify-between gap-[5px] truncate"
                 key={`receipt-${index}`}>
                 <Input
-                  variant="underline"
-                  {...register(`receiptArr.${index}.food_category`)}
+                  variant={
+                    errors?.receiptItems?.[index]?.food_category
+                      ? 'danger'
+                      : 'underline'
+                  }
+                  {...register(`receiptItems.${index}.food_category`, {
+                    required: {
+                      value: true,
+                      message: '빈 칸이 없게 작성해주세요',
+                    },
+                    minLength: 1,
+                  })}
                   className="basis-2/12"
                 />
                 <Input
-                  variant="underline"
-                  {...register(`receiptArr.${index}.food_name`)}
+                  variant={
+                    errors?.receiptItems?.[index]?.food_name
+                      ? 'danger'
+                      : 'underline'
+                  }
+                  {...register(`receiptItems.${index}.food_name`, {
+                    required: {
+                      value: true,
+                      message: '빈 칸이 없게 작성해주세요',
+                    },
+                    minLength: 1,
+                  })}
                   className="max-w-[127px] basis-5/12 truncate"
                 />
                 <Input
-                  variant="underline"
-                  {...register(`receiptArr.${index}.food_weight`)}
+                  variant={
+                    errors?.receiptItems?.[index]?.food_weight
+                      ? 'danger'
+                      : 'underline'
+                  }
+                  {...register(`receiptItems.${index}.food_weight`, {
+                    required: {
+                      value: true,
+                      message: '빈 칸이 없게 작성해주세요',
+                    },
+                    minLength: 1,
+                  })}
                   className="basis-2/12"
                 />
                 <Input
-                  variant="underline"
-                  {...register(`receiptArr.${index}.purchase_price`)}
+                  variant={
+                    errors?.receiptItems?.[index]?.purchase_price
+                      ? 'danger'
+                      : 'underline'
+                  }
+                  {...register(`receiptItems.${index}.purchase_price`, {
+                    required: {
+                      value: true,
+                      message: '빈 칸이 없게 작성해주세요',
+                    },
+                    min: 1,
+                  })}
                   className="basis-2/12"
                 />
                 <div className="basis-1/12 cursor-pointer">
-                  <MinusSvg onClick={() => remove(index)} />
+                  {index !== 0 && <MinusSvg onClick={() => remove(index)} />}
                 </div>
               </div>
             ))}
-          </form>
-          <div className="flex gap-[5px]">
-            <div className="basis-11/12"></div>
-            <PlusSvg
-              className="flex basis-1/12"
-              onClick={() =>
-                append({
-                  food_id: Math.random() * 4,
-                  food_category: '',
-                  food_name: '',
-                  food_weight: '',
-                  purchase_price: 0,
-                  quantity: 0,
-                  registered: false,
-                })
-              }
-            />
+            <div className="flex gap-[5px]">
+              <div className="basis-11/12"></div>
+              <PlusSvg
+                className="flex basis-1/12"
+                onClick={() =>
+                  append({
+                    food_id: Math.random() * 4,
+                    food_category: undefined,
+                    food_name: undefined,
+                    food_weight: undefined,
+                    purchase_price: undefined,
+                    quantity: undefined,
+                    registered: false,
+                  })
+                }
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex justify-end gap-[30px]">
-          <div>품목 {purchaseReceiptInfo[0].receiptItems.length}개</div>
-          <div>총{totalPrice.toLocaleString()}원</div>
-        </div>
-        <Button
-          onClick={onSubmit}
-          variant="outlined"
-          className="w-full rounded-lg">
-          수정완료
-        </Button>
+          <div className="flex justify-end gap-[30px]">
+            <div>품목 {length}개</div>
+            <div>총{totalPrice ? totalPrice.toLocaleString() : 0}원</div>
+          </div>
+          <div>{errors && <p>{errors?.root?.message}</p>}</div>
+          <Button
+            type="submit"
+            variant="outlined"
+            className="w-full rounded-lg">
+            추가하기
+          </Button>
+        </form>
       </Card>
     </div>
   );
