b450ebf6abaad2e5c3d159176cd6838fe51f31fc
Jamee <codingjamee@gmail.com>
2024-03-25 22:24:32 +0900

    Feat: modal portal을 위해

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MSWComponent />
+        <div id="modal-root" />
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
