24d1a27ed8f7138325518a967975cd66ce637081
Jamee <codingjamee@gmail.com>
2024-03-25 22:23:49 +0900

    Feat: react-hook-form으로 등록할 수 있도록 prop타입 옵셔널로 type은 UseFormRegister

--- src/app/(afterLogin)/_component/Search.tsx
+++ src/app/(afterLogin)/_component/Search.tsx
@@ -1,11 +1,24 @@
 import SearchGlass from '@/_assets/SearchGlass';
 import Input from '@/_components/Input';
+import { FieldValues, UseFormRegister } from 'react-hook-form';
 
-const Search = () => {
+interface SearchProps {
+  onClickSearch?: () => void;
+  register?: UseFormRegister<FieldValues>;
+  name?: string;
+}
+
+const Search = ({ onClickSearch, register, name }: SearchProps) => {
   return (
     <div className="flex w-full items-center justify-center">
-      <Input variant="underline" placeholder="재료명" />
-      <div className="h-[18px] w-[29px] translate-x-[-30px] translate-y-[-5px] cursor-pointer">
+      <Input
+        {...(register && name ? register(name) : {})}
+        variant="underline"
+        placeholder="재료명"
+      />
+      <div
+        onClick={onClickSearch}
+        className="h-[18px] w-[29px] translate-x-[-30px] translate-y-[-5px] cursor-pointer">
         <SearchGlass />
       </div>
     </div>
