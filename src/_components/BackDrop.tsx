f4ebca9d04a16e049eb503b35fa9e3e757bdbea6
Jamee <codingjamee@gmail.com>
2024-03-26 10:18:40 +0900

    Disign: 백드롭이 전체 화면을 감싸지 않는 문제 해결

--- src/_components/BackDrop.tsx
+++ src/_components/BackDrop.tsx
@@ -7,7 +7,7 @@ type Props = {
 const BackDrop: FC<PropsWithChildren<Props>> = ({ children, onClick }) => {
   return (
     <div
-      className="absolute z-[999] flex h-full w-full items-center justify-center bg-color-bg-main "
+      className="backdrop fixed left-0  top-0 z-[999] flex h-full w-full items-center justify-center"
       onClick={onClick}>
       {children}
     </div>
