e1a3f5c7269871c3e758ccb87f78cb01f0ee42f0
Jamee <codingjamee@gmail.com>
2024-03-27 00:29:39 +0900

    Feat: id가 있는경우와 없는경우 쿼리 나눠서 실행

--- src/_utils/postQuery.ts
+++ src/_utils/postQuery.ts
@@ -1,5 +1,6 @@
 import { PurchaseReceiptInfoType } from '@/_types/ReceiptTypes';
 import { api } from './createCustomFetch';
+import { FoodPropType } from '@/_types/FoodTypes';
 
 export const postReceiptData = async (
   data: PurchaseReceiptInfoType | undefined,
@@ -16,4 +17,29 @@ export const postReceiptData = async (
   }
 
   return res.json();
+};
+
+export const postFoodDataById = async (data: FoodPropType, foodId: string) => {
+  const res = await api(`/food/${foodId}`, {
+    next: {
+      tags: ['addFood', foodId],
+    },
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    throw new Error('Failed to post food data!');
+  }
+  return res.json();
+};
+
+export const postFoodData = async (data: FoodPropType) => {
+  const res = await api('/food', {
+    next: {
+      tags: ['addFirstFood'],
+    },
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+  return res;
 };
