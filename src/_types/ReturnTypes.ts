83dc3a8926c4f482312536fbf5ad2429caa69279
Jamee <codingjamee@gmail.com>
2024-03-26 10:14:13 +0900

    Feat: naver search 결과 return type 설정

--- src/_types/ReturnTypes.ts
+++ src/_types/ReturnTypes.ts
@@ -73,3 +73,28 @@ export interface AnalyzedReceiptAllType {
     };
   };
 }
+
+export interface SearchReturnType {
+  lastBuildDate: string;
+  total: number;
+  start: number;
+  display: number;
+  items: SearchItems[];
+}
+
+interface SearchItems {
+  title: string;
+  link: string;
+  image: string;
+  lprice: string;
+  hprice: string;
+  mallName: string;
+  productId: string;
+  productType: string;
+  brand: string;
+  maker: string;
+  category1: string;
+  category2: string;
+  category3: string;
+  category4: string;
+}
