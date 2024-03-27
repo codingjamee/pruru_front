9d2f010f40620f15e230774a8b43a51b28ed31b0
Jamee <codingjamee@gmail.com>
2024-03-27 18:17:39 +0900

    Feat: /food로만 가져오고 데이터 캐싱하기
    커스텀훅으로 소트 및 필터링

--- src/app/(afterLogin)/_component/ExpiryFood.tsx
+++ src/app/(afterLogin)/_component/ExpiryFood.tsx
@@ -6,26 +6,34 @@ import FoodCard from './FoodCard';
 import SmallFoodCard from './SmallFoodCard';
 import { useSearchParams } from 'next/navigation';
 import { QueryTypes } from '@/_types/CommonTypes';
+import useSortByQuery from '../_hooks/useSortByQuery';
 
 const ExpiryFood = () => {
   const params = useSearchParams();
   const storage: QueryTypes['storage'] =
     (params.get('storage') as QueryTypes['storage']) || 'total';
   const sort: QueryTypes['sort'] =
-    (params.get('sort') as QueryTypes['sort']) || 'expiryDate';
+    (params.get('sort') as QueryTypes['sort']) || 'expiry_date';
   const direction: QueryTypes['direction'] =
     (params.get('direction') as QueryTypes['direction']) || 'down';
-
-  const { data: expiryFood } = useQuery({
-    queryKey: ['foods', storage, sort, direction],
-    queryFn: () => getFoods({ storage, sort, direction }),
+  const { data: expiryFood } = useQuery<FoodPropType[]>({
+    queryKey: ['foods'],
+    queryFn: () => getFoods(),
     staleTime: 10 * 60 * 1000,
   });
 
+  const sortedData = useSortByQuery(
+    {
+      sort,
+      direction,
+      storage,
+    },
+    expiryFood,
+  );
   return (
     <>
-      {expiryFood &&
-        expiryFood.map((food: FoodPropType) => (
+      {sortedData &&
+        sortedData.map((food: FoodPropType) => (
           <div key={food.id}>
             <FoodCard className="mobile:hidden" food={food} />
             <SmallFoodCard
