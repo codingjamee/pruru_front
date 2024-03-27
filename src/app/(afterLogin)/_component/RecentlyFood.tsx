1a5dbc5a84df74b3b863368945ad97cfd66e7258
Jamee <codingjamee@gmail.com>
2024-03-27 22:14:10 +0900

    Feat: 캐싱하고 useSortByQuery로 정렬된 카드 데이터 보여주기 기능

--- src/app/(afterLogin)/_component/RecentlyFood.tsx
+++ src/app/(afterLogin)/_component/RecentlyFood.tsx
@@ -1,19 +1,35 @@
 'use client';
 import { FoodPropType } from '@/_types/FoodTypes';
-import { foodsByPurchase } from '@/_utils/getQuery';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
 import FoodCard from './FoodCard';
 import SmallFoodCard from './SmallFoodCard';
+import { getFoods } from '@/_utils/getQuery';
+import useSortByQuery from '../_hooks/useSortByQuery';
 
 const RecentlyFood = () => {
-  const { data: purchaseFood } = useQuery({
-    queryKey: ['foods', 'purchase_date'],
-    queryFn: foodsByPurchase,
+  const queryClient = useQueryClient();
+
+  const { data: recentFood, isSuccess } = useQuery({
+    queryKey: ['foods'],
+    queryFn: getFoods,
+    staleTime: 10 * 60 * 1000,
+    initialData: queryClient.getQueryData(['foods']),
   });
+
+  const sortedData = useSortByQuery(
+    {
+      sort: 'purchaseDate',
+      direction: 'up',
+      storage: 'total',
+    },
+    isSuccess,
+    recentFood,
+  );
+
   return (
     <>
-      {purchaseFood?.map((food: FoodPropType) => (
+      {sortedData?.map((food: FoodPropType) => (
         <React.Fragment key={food.id}>
           <FoodCard className="mobile:hidden" food={food} />
           <SmallFoodCard className="tablet:hidden desktop:hidden" food={food} />
