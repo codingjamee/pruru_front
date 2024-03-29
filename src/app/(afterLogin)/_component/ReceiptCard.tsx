e8e06ad7ca9f7371c3f152d485fef872b96d50fa
Jamee <codingjamee@gmail.com>
2024-03-30 01:18:38 +0900

    Fix: 변수수정

--- src/app/(afterLogin)/_component/ReceiptCard.tsx
+++ src/app/(afterLogin)/_component/ReceiptCard.tsx
@@ -9,7 +9,7 @@ import { useYearMonthCtx } from '@/_contexts/DateContext';
 const ReceiptCard = () => {
   const router = useRouter();
   const { yearMonth } = useYearMonthCtx();
-  const { data: receipts } = useQuery({
+  const { data: receiptData } = useQuery({
     queryKey: ['receipt', 'monthly', yearMonth],
     queryFn: () => getReceiptsByMonth(yearMonth),
     staleTime: 60 * 1000,
@@ -19,20 +19,20 @@ const ReceiptCard = () => {
   };
   return (
     <>
-      {receipts &&
-        receipts.map((receipt: ReceiptArrType) => (
+      {receiptData &&
+        receiptData.receipts.map((data: ReceiptArrType) => (
           <Card
-            onClick={() => onClickCard(receipt.receipt_id)}
-            key={receipt.receipt_id}
+            onClick={() => onClickCard(data.id)}
+            key={data.id}
             variant="outlined"
             className="flex w-full flex-shrink cursor-pointer justify-between gap-[20px] px-[40px]">
             <div className="flex w-full justify-between">
-              <div>{receipt.purchase_date} 구매 영수증</div>
-              <div>{receipt.purchase_location}</div>
+              <div>{data.purchase_date} 구매 영수증</div>
+              <div>{data.purchase_location}</div>
             </div>
             <div className="flex w-full justify-between">
-              <div>품목 {receipt.quantity}개</div>
-              <div>총 {receipt.total_price.toLocaleString()}원</div>
+              <div>품목 {data.quantity}개</div>
+              <div>총 {data.total_price.toLocaleString()}원</div>
             </div>
           </Card>
         ))}
