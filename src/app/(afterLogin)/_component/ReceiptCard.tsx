3c3b1d7c19af08c26f14e554ff13a9bdffe51abc
Jamee <codingjamee@gmail.com>
2024-03-30 17:45:02 +0900

    Feat: useInfiniteQuery기능 및 타입 정의 returnType변경으로 로직 재정의

--- src/app/(afterLogin)/_component/ReceiptCard.tsx
+++ src/app/(afterLogin)/_component/ReceiptCard.tsx
@@ -1,41 +1,73 @@
 'use client';
 import Card from '@/_components/Card';
-import { ReceiptArrType } from '@/_types/ReceiptTypes';
+import { ReceiptArrType, ReceiptsReturnType } from '@/_types/ReceiptTypes';
 import { useRouter } from 'next/navigation';
-import { useQuery } from '@tanstack/react-query';
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
 import { getReceiptsByMonth } from '@/_utils/getQuery';
 import { useYearMonthCtx } from '@/_contexts/DateContext';
+import { useRef } from 'react';
+import useIntersectionObserver from '../_hooks/useIntersectionObserver';
+import dayjs from 'dayjs';
 
 const ReceiptCard = () => {
   const router = useRouter();
   const { yearMonth } = useYearMonthCtx();
-  const { data: receiptData } = useQuery({
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { data: receiptData, fetchNextPage } = useInfiniteQuery<
+    ReceiptsReturnType,
+    unknown,
+    InfiniteData<ReceiptsReturnType>,
+    [_1: string, _2: string, _3: string],
+    unknown
+  >({
     queryKey: ['receipt', 'monthly', yearMonth],
-    queryFn: () => getReceiptsByMonth(yearMonth),
-    staleTime: 60 * 1000,
+    queryFn: () =>
+      getReceiptsByMonth({
+        YM: yearMonth || dayjs().format('YY.MM'),
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (data) => {
+      data.nextCursor;
+    },
   });
+
+  const onIntersect: IntersectionObserverCallback = async ([
+    { isIntersecting },
+  ]) => {
+    await fetchNextPage();
+    console.log('인터섹션이 감지됨', isIntersecting);
+  };
+
+  useIntersectionObserver({
+    target: targetRef.current,
+    onIntersect: onIntersect,
+  });
+
   const onClickCard = (id: number) => {
     router.push(`/receipt/${id}`);
   };
   return (
     <>
       {receiptData &&
-        receiptData.receipts.map((data: ReceiptArrType) => (
-          <Card
-            onClick={() => onClickCard(data.id)}
-            key={data.id}
-            variant="outlined"
-            className="flex w-full flex-shrink cursor-pointer justify-between gap-[20px] px-[40px]">
-            <div className="flex w-full justify-between">
-              <div>{data.purchase_date} 구매 영수증</div>
-              <div>{data.purchase_location}</div>
-            </div>
-            <div className="flex w-full justify-between">
-              <div>품목 {data.quantity}개</div>
-              <div>총 {data.total_price.toLocaleString()}원</div>
-            </div>
-          </Card>
-        ))}
+        receiptData.pages.map((page: ReceiptsReturnType) =>
+          page.receipts?.map((data: ReceiptArrType) => (
+            <Card
+              onClick={() => onClickCard(data.id)}
+              key={data.id}
+              variant="outlined"
+              className="flex w-full flex-shrink cursor-pointer justify-between gap-[20px] px-[40px]">
+              <div className="flex w-full justify-between">
+                <div>{data.purchase_date} 구매 영수증</div>
+                <div>{data.purchase_location}</div>
+              </div>
+              <div className="flex w-full justify-between">
+                <div>품목 {data.quantity}개</div>
+                <div>총 {data.total_price.toLocaleString()}원</div>
+              </div>
+            </Card>
+          )),
+        )}
+      <div ref={targetRef} />
     </>
   );
 };
