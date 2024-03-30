f79195e9e38fe42b45ba925d9978e0e381516744
Jamee <codingjamee@gmail.com>
2024-03-30 23:49:02 +0900

    Feat: 받아온 데이터 형식 수정 및 setQueryData 설정완료

--- src/app/(afterLogin)/_component/ReceiptDetailCard.tsx
+++ src/app/(afterLogin)/_component/ReceiptDetailCard.tsx
@@ -3,17 +3,27 @@ import PlusSvg from '@/_assets/PlusSvg';
 import RefrigerIcon from '@/_assets/RefrigerIcon';
 import Button from '@/_components/Button';
 import Card from '@/_components/Card';
-import { ReceiptDetailType } from '@/_types/ReceiptTypes';
-import { getreceipt_items } from '@/_utils/getQuery';
+import {
+  PurchaseReceiptInfoType,
+  ReceiptDetailType,
+} from '@/_types/ReceiptTypes';
+import { getReceiptDetail } from '@/_utils/getQuery';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import dayjs from 'dayjs';
 
 const ReceiptDetailCard = ({ receipt_id }: { receipt_id: string }) => {
   const router = useRouter();
 
-  const { data: purchaseReceiptInfo } = useQuery({
+  const { data: purchaseReceiptInfo } = useQuery<
+    PurchaseReceiptInfoType,
+    any,
+    PurchaseReceiptInfoType,
+    any
+  >({
     queryKey: ['receipt', 'items', receipt_id],
-    queryFn: () => getreceipt_items(receipt_id),
+    queryFn: () => getReceiptDetail(receipt_id),
+    staleTime: 10 * 60 * 1000,
   });
   const queryClient = useQueryClient();
 
@@ -21,10 +31,10 @@ const ReceiptDetailCard = ({ receipt_id }: { receipt_id: string }) => {
   console.log(purchaseReceiptInfo);
 
   const onClickAddFood = (index: number, foodId?: string) => {
-    if (foodId) {
+    if (foodId && purchaseReceiptInfo) {
       queryClient.setQueryData(
         ['addFood', foodId],
-        purchaseReceiptInfo[0][index],
+        purchaseReceiptInfo.receipt_items[index],
       );
       router.push(`/add/food?foodId=${foodId}`);
     }
@@ -41,10 +51,10 @@ const ReceiptDetailCard = ({ receipt_id }: { receipt_id: string }) => {
           className="flex min-h-[695px] w-[635px] flex-col gap-[30px] px-[55px] mobile:min-h-[450px] mobile:border-0 mobile:px-4">
           <div className="flex w-full justify-between gap-[10px] mobile:flex-col">
             <div className="w-[213px] rounded-lg border border-solid border-color-default-text px-[30px] py-[7px] text-center mobile:w-full">
-              {purchaseReceiptInfo[0].purchase_date} 구매
+              {dayjs(purchaseReceiptInfo.purchase_date).format('YY.MM.DD')} 구매
             </div>
             <Button variant="primary" className="rounded-lg mobile:w-full">
-              {purchaseReceiptInfo[0].purchase_location}
+              {purchaseReceiptInfo.purchase_location}
             </Button>
           </div>
           <div className="flex flex-col gap-[10px] mobile:gap-[15px]">
@@ -55,11 +65,11 @@ const ReceiptDetailCard = ({ receipt_id }: { receipt_id: string }) => {
               <div className="basis-2/12">금액</div>
               <div className="basis-1/12">등록</div>
             </div>
-            {purchaseReceiptInfo[0].receipt_items.map(
+            {purchaseReceiptInfo.receipt_items.map(
               (receipt: ReceiptDetailType, index: number) => (
                 <div
                   className="flex w-full justify-between truncate"
-                  key={receipt.food_id}>
+                  key={receipt.id}>
                   <div className="basis-2/12">{receipt.category}</div>
                   <div className="max-w-[127px] basis-5/12 truncate">
                     {receipt.name}
@@ -76,7 +86,7 @@ const ReceiptDetailCard = ({ receipt_id }: { receipt_id: string }) => {
                     ) : (
                       <PlusSvg
                         onClick={() =>
-                          onClickAddFood(index, receipt.food_id?.toString())
+                          onClickAddFood(index, receipt.id?.toString())
                         }
                       />
                     )}
@@ -87,10 +97,8 @@ const ReceiptDetailCard = ({ receipt_id }: { receipt_id: string }) => {
           </div>
 
           <div className="flex justify-end gap-[30px]">
-            <div>품목 {purchaseReceiptInfo[0].receipt_items.length}개</div>
-            <div>
-              총{purchaseReceiptInfo[0].total_price?.toLocaleString()}원
-            </div>
+            <div>품목 {purchaseReceiptInfo.receipt_items.length}개</div>
+            <div>총{purchaseReceiptInfo.total_price?.toLocaleString()}원</div>
           </div>
         </Card>
       )}
