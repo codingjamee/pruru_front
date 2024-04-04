61e1f062dd242bc5b049407974ee1abbbbcb0fdf
Jamee <codingjamee@gmail.com>
2024-04-04 14:02:05 +0900

    Fix: 최근 산 재료 가져오기 getFoods에 query string 건네주기

--- src/app/(afterLogin)/_component/RecentlyFood.tsx
+++ src/app/(afterLogin)/_component/RecentlyFood.tsx
@@ -5,36 +5,39 @@ import React from 'react';
 import FoodCard from './FoodCard';
 import SmallFoodCard from './SmallFoodCard';
 import { getFoods } from '@/_utils/getQuery';
-import useSortByQuery from '../_hooks/useSortByQuery';
 
 const RecentlyFood = () => {
   const queryClient = useQueryClient();
+  const storage = 'total';
+  const sort = 'purchaseDate';
+  const direction = 'up';
 
   const { data: recentFood, isSuccess } = useQuery({
     queryKey: ['foods'],
-    queryFn: getFoods,
+    queryFn: ({ pageParam }) =>
+      getFoods({
+        storage,
+        sort,
+        direction,
+        pageParam,
+      }),
     staleTime: 10 * 60 * 1000,
     initialData: queryClient.getQueryData(['foods']),
   });
 
-  const sortedData = useSortByQuery(
-    {
-      sort: 'purchaseDate',
-      direction: 'up',
-      storage: 'total',
-    },
-    isSuccess,
-    recentFood,
-  );
-
   return (
     <>
-      {sortedData?.map((food: FoodPropType) => (
-        <React.Fragment key={food.id}>
-          <FoodCard className="mobile:hidden" food={food} />
-          <SmallFoodCard className="tablet:hidden desktop:hidden" food={food} />
-        </React.Fragment>
-      ))}
+      {isSuccess &&
+        recentFood &&
+        recentFood.foods.map((food: FoodPropType) => (
+          <React.Fragment key={food.id}>
+            <FoodCard className="mobile:hidden" food={food} />
+            <SmallFoodCard
+              className="tablet:hidden desktop:hidden"
+              food={food}
+            />
+          </React.Fragment>
+        ))}
     </>
   );
 };
