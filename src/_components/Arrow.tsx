807687fd3dfd229afe8fc9f7b9e1f3f92d53bed5
Jamee <codingjamee@gmail.com>
2024-04-04 13:56:14 +0900

    Fix: 타입 설정 및 return arrow svg로 변경

--- src/_components/Arrow.tsx
+++ src/_components/Arrow.tsx
@@ -1,16 +1,21 @@
 'use client';
-import { useEffect, useState } from 'react';
+import LeftSvg from '@/_assets/LeftSvg';
+import RightSvg from '@/_assets/RightSvg';
+import { FC, PropsWithChildren, useEffect, useState } from 'react';
 
-const Arrow = ({
+interface ArrowProps {
+  direction: 'left' | 'right';
+  executeFn: (direction: 'left' | 'right') => void;
+  inArrow?: boolean;
+  hoverStyle?: string;
+}
+
+const Arrow: FC<PropsWithChildren<ArrowProps>> = ({
   direction,
   executeFn,
   inArrow,
   hoverStyle,
-}: {
-  direction: 'left' | 'right';
-  executeFn: (val: 'left' | 'right') => void;
-  inArrow?: boolean;
-  hoverStyle?: string;
+  children,
 }) => {
   const [hoverColor, setHoverColor] = useState(hoverStyle || undefined);
 
@@ -18,11 +23,14 @@ const Arrow = ({
     hoverStyle && setHoverColor(`hover:${hoverStyle}`);
   }, [hoverStyle]);
 
+  const returnString = direction === 'left' ? <LeftSvg /> : <RightSvg />;
+
   return (
     <div
       onClick={() => executeFn(direction)}
       className={`${inArrow ? 'absolute z-50' : ''} ${direction === 'left' ? 'left-0 top-0 rounded-l-lg' : 'right-0 top-0 rounded-r-lg'}  center-vertical flex h-full w-[30px] cursor-pointer text-[30px] ${hoverColor && hoverColor}`}>
-      {direction === 'left' ? '<' : '>'}
+      {returnString}
+      {children}
     </div>
   );
 };
