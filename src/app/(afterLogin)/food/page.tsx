d763a0fa978f399b62665fd3fc84a491c7e3034e
Jamee <codingjamee@gmail.com>
2024-04-18 18:15:24 +0900

    Feat: food페이지에서 식재료 추가하기 기능 추가

--- src/app/(afterLogin)/food/page.tsx
+++ src/app/(afterLogin)/food/page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import QueryComponent from '../_component/QueryComponent';
 import Foods from '../_component/Foods';
 import { Metadata } from 'next';
+import Card from '@/_components/Card';
+import Button from '@/_components/Button';
+import PlusSvg from '@/_assets/PlusSvg';
 
 export const metadata: Metadata = {
   title: 'Food / PRURU',
@@ -21,6 +24,16 @@ const page = () => {
           <QueryComponent query="direction" />
         </div>
       </div>
+      <Card variant="primary" className="w-full flex-shrink justify-between">
+        <div className="px-[10px] text-size-font-card-title">
+          식재료 추가하기
+        </div>
+        <Button
+          href="/add/food"
+          className="flex h-[50%] w-full flex-col items-center justify-center rounded-lg hover:bg-color-primary-m">
+          <PlusSvg className="flex h-[44px] w-[44px] items-center justify-center" />
+        </Button>
+      </Card>
       <div className="flex flex-row flex-wrap items-center justify-center gap-[10px]">
         <Foods />
       </div>
