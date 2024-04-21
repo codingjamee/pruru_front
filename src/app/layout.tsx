95c0f1486d9c7ab2e9c01b6bee85833cb6f2adb4
Jamee <codingjamee@gmail.com>
2024-04-21 16:20:05 +0900

    Feat: manifest 적용해보기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,41 @@ import { RQProvider } from './(afterLogin)/_component/RQProvider';
 import { ToastProvider } from '@/_components/Toast';
 
 export const metadata: Metadata = {
-  title: 'PRURU',
-  description: '당신의 냉장고를 신선하게',
+  title: 'PRURU refrigerator manage App',
+  description: '당신의 냉장고를 신선하게...',
+  themeColor: '#7bc35b',
+  icons: [
+    {
+      url: '/icons/android-icon-36x36.png',
+      sizes: '36x36',
+      type: 'image/png',
+    },
+    {
+      url: '/icons/android-icon-48x48.png',
+      sizes: '48x48',
+      type: 'image/png',
+    },
+    {
+      url: '/icons/android-icon-72x72.png',
+      sizes: '72x72',
+      type: 'image/png',
+    },
+    {
+      url: '/icons/android-icon-96x96.png',
+      sizes: '96x96',
+      type: 'image/png',
+    },
+    {
+      url: '/icons/android-icon-144x144.png',
+      sizes: '144x144',
+      type: 'image/png',
+    },
+    {
+      url: '/icons/android-icon-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+  ],
 };
 
 export default function RootLayout({
